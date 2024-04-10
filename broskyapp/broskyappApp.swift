48fdbc8a30477f26199b75c7dec77c461ce73847
Marek Dlabaja <marecek.dlabaja@gmail.com>
2024-04-10 11:58:14 +0200

    Initial Commit

---
+++ broskyapp/broskyappApp.swift
@@ -0,0 +1,17 @@
+//
+//  broskyappApp.swift
+//  broskyapp
+//
+//  Created by Marek Dlabaja on 10.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct broskyappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
